3f0e8977e20c0af86f26b5d5b36f8a6a02d46d83
Taeyoun Kim <86701096+taeyounkim1107@users.noreply.github.com>
2024-07-09 08:53:26 +0900

    Create 25_Adder 1.v

---
+++ 25_Adder 1.v
@@ -0,0 +1,10 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+	wire one, two;
+    add16 inst1(a[15:0], b[15:0], 0, sum[15:0], one);
+    add16 inst2(a[31:16], b[31:16], one, sum[31:16], two);
+    
+endmodule
