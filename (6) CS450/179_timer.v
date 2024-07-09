8ba0f90832a2cfe9ebb09407aaeb3c602d8882fd
Taeyoun Kim <86701096+taeyounkim1107@users.noreply.github.com>
2024-07-09 16:23:55 +0900

    Update 179_timer.v

--- (6) CS450/179_timer.v
+++ (6) CS450/179_timer.v
@@ -9,23 +9,23 @@ module top_module(
     
     always @ (posedge clk) begin
 		if (load) begin
-        	counter = data;            
-            if (counter == 0) begin tc = 1; end
-            else begin tc = 0; end
-        end
+        		counter = data;            
+            		if (counter == 0) begin tc = 1; end
+            		else begin tc = 0; end
+        	end
         
-        else begin
-            if (counter > 1) begin
-            	counter = counter - 1;
-            end
-            else if (counter == 1) begin
-            	counter = counter - 1;
-                tc = 1;
-            end
-            else begin
-            	counter = counter;
-            end
-        end
+        	else begin
+            		if (counter > 1) begin
+            			counter = counter - 1;
+            		end
+            		else if (counter == 1) begin
+            			counter = counter - 1;
+                		tc = 1;
+            		end
+            		else begin
+            			counter = counter;
+            		end
+        	end
     end
 
 endmodule
